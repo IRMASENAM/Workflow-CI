5dec6b86024c534480028602e8fdef6d1c80db79
yusuf version <yusufef698@gmail.com>
2026-03-12 12:58:36 +0700

    fix modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,17 +6,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-# Aktifkan autolog
 mlflow.sklearn.autolog()
 
-# Load dataset
-df = pd.read_csv("heart_preprocessing.csv")
+df = pd.read_csv("MLProject/heart_preprocessing.csv")
 
-# Pisahkan fitur dan target
 X = df.drop("target", axis=1)
 y = df["target"]
 
-# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -24,25 +20,19 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-# Start MLflow run
 with mlflow.start_run():
 
-    # Model
     model = LogisticRegression(max_iter=1000)
 
-    # Training
     model.fit(X_train, y_train)
 
-    # Predict
     y_pred = model.predict(X_test)
 
-    # Evaluasi
     acc = accuracy_score(y_test, y_pred)
 
     print("Accuracy:", acc)
 
-    # Manual logging
     mlflow.log_metric("accuracy_manual", acc)
 
-    # Log model artifact
+    # WAJIB supaya docker bisa build model
     mlflow.sklearn.log_model(model, "model")
